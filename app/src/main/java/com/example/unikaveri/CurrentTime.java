9eb05524a319f042b61ee86edb57ec06a4ccf889
ysi <33661922+yliysi@users.noreply.github.com>
2022-04-21 14:46:57 +0300

    frontpage update
    Frontpage update, following wireframe - date and greeting text OK (except username)
    
    CurrentTime class added - needs more strings for different times in greetingTextTime method

---
+++ app/src/main/java/com/example/unikaveri/CurrentTime.java
@@ -0,0 +1,61 @@
+package com.example.unikaveri;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class CurrentTime {
+
+    private Calendar c;
+    private String currentTime;
+
+    /* get current time */
+    public CurrentTime(){
+        this.c = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+        this.currentTime = sdf.format(c.getTime());
+    }
+
+    /* return current time */
+    public String getCurrentTime(){
+        return this.currentTime;
+    }
+
+    /* return current time in hours */
+    public int getCurrentHour(){
+        String hour = new SimpleDateFormat("HH").format(new Date());
+        int currentHour = Integer.parseInt(hour);
+
+        return currentHour;
+    }
+
+    /* return current weekday */
+    public String getWeekday(){
+        String weekday = new SimpleDateFormat("EEEE").format(new Date());
+        return weekday;
+    }
+
+    /* return current date */
+    public String getDate(){
+        String date = new SimpleDateFormat("d.M").format(new Date());
+        return date;
+    }
+
+    /* return appropriate greetingText string */
+    public String greetingTextTime(int hour){
+        if(hour >= 18 && hour <= 3){
+            return "iltaa";
+        }
+
+        if(hour >= 3 && hour <= 9){
+            return "huomenta";
+        }
+
+        if(hour >= 10 && hour <= 17){
+            return "päivää";
+        }
+
+        return "date unspecified";
+    }
+
+}
--- app/src/main/java/com/example/unikaveri/MainActivity.java
+++ app/src/main/java/com/example/unikaveri/MainActivity.java
@@ -2,22 +2,37 @@ package com.example.unikaveri;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
+import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private CurrentTime time;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    /** Called when the user taps the Send button */
-    public void sendMessage(View view) {
-        Intent intent = new Intent(this, Activity2Test.class);
-        startActivity(intent);
+        time = new CurrentTime();
+        //test
+        //Log.d("date",time);
+
+        Log.d("date","hour: "+time.getCurrentHour());
+
+        /* Edit greeting text */
+        TextView editGreeting = (TextView) findViewById(R.id.greetingText);
+        // get appropriate greeting based on current hour
+        editGreeting.setText("Hyvää "+time.greetingTextTime(time.getCurrentHour())+"!");
+
+        /* Edit clock time */
+        TextView editClock = (TextView) findViewById(R.id.textClock);
+        editClock.setText(time.getCurrentTime());
+
+        /* Edit date & weekday */
+        TextView editWeekday = (TextView) findViewById(R.id.weekdayText);
+        editWeekday.setText(time.getDate()+" "+time.getWeekday());
+
     }
 }
