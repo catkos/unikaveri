3d99d4449e18d4e13c6b768cc1b477d956f733a1
ysi <33661922+yliysi@users.noreply.github.com>
2022-04-21 11:23:35 +0300

    2nd activity test

---
+++ app/src/main/java/com/example/unikaveri/Activity2Test.java
@@ -0,0 +1,14 @@
+package com.example.unikaveri;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity2Test extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activity2_test);
+    }
+}
--- app/src/main/java/com/example/unikaveri/MainActivity.java
+++ app/src/main/java/com/example/unikaveri/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.unikaveri;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, Activity2Test.class);
+        startActivity(intent);
     }
 }
